bd222974910e7fba26f762587c398587595737ae
Andrew Crisp <andrew.crisp@gmail.com>
2022-11-08 20:23:51 -0700

    Generate nested array of positions by ReportsTo

--- app/excel_functions.js
+++ app/excel_functions.js
@@ -49,23 +49,24 @@ function parseExcel(file) {
   var reader = new FileReader();
 
   reader.onload = function(e) {
-	var data = e.target.result;
-	var workbook = XLSX.read(data, {
-	  type: 'binary'
-	});
-	workbook.SheetNames.forEach(function(sheetName) {
-	  // Here is your object
-	  var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-	  var json_object = JSON.stringify(XL_row_object);
-	  console.log(JSON.parse(json_object));
-	  jQuery('#xlx_json').val(json_object);
-	  mytable = XL_row_object;
-	  myobjects = convert(mytable)
-	})
+    var data = e.target.result;
+    var workbook = XLSX.read(data, {
+      type: 'binary'
+    });
+    workbook.SheetNames.forEach(function(sheetName) {
+      // Here is your object
+      var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+      var json_object = JSON.stringify(XL_row_object);
+      console.log(JSON.parse(json_object));
+      jQuery('#xlx_json').val(json_object);
+      mytable = XL_row_object;
+      //myobjects = convert(mytable)
+	  myobjects = SelectFields(mytable)
+    })
   };
 
   reader.onerror = function(ex) {
-	console.log(ex);
+    console.log(ex);
   };
 
   reader.readAsBinaryString(file);
--- app/utils.js
+++ app/utils.js
@@ -1,18 +1,69 @@
 function SelectFields(arr) {
-var temparray = []
-arr.forEach(function(item){
-	temp = {
-		ID: item['DeptID'],
-		Position: item['Position'],
-		DeptID: item['DeptID'],
-		Name: item['Name'],
-		PDNumber: item['Master Record #'],
-		Title: item['Job Title'],
-		ReportsTo: item['Reports To'],
-		PayCode: item['Pay Plan'] + '-' + item['Occ Series'] + '-' + item['Grade']
+    var temparray = []
+    arr.forEach(function(item){
+        //temp = {
+        //    ID: item['DeptID'],
+        //    Position: item['Position'],
+        //    DeptID: item['DeptID'],
+        //    name: item['Name'],
+        //    PDNumber: item['Master Record #'],
+        //    Title: item['Job Title'],
+        //    ReportsTo: item['Reports To'],
+        //    PayCode: item['Pay Plan'] + '-' + item['Occ Series'] + '-' + item['Grade'],
+		//	children: []
+        //}
+		temp = new Position(item)
+        temparray.push(temp)
+        //console.log(item['Job Title'])
+    })
+    return temparray
+}
+
+class Position{
+	constructor(item) {
+        this.ID = item['DeptID'],
+        this.Position = item['Position'],
+        this.DeptID = item['DeptID'],
+        this.name = item['Name'],
+        this.PDNumber = item['Master Record #'],
+        this.Title = item['Job Title'],
+        this.ReportsTo = item['Reports To'],
+        this.PayCode = item['Pay Plan'] + '-' + item['Occ Series'] + '-' + item['Grade'],
+        this.children = []
+	    this.AddChildren = function(arr){
+			this.children = arr.filter(i => i['ReportsTo'] == this.Position)
+			if(this.children.length > 0){
+				this.children.forEach(c => c.AddChildren(arr))
+			}
+	    }
 	}
-	temparray.push(temp)
-	//console.log(item['Job Title'])
-})
-return temparray
+}
+
+function GetParents(arr) {
+    var parents = arr.map(function(d) { return d['ReportsTo']; });
+	
+	var unique = parents.filter(onlyUnique);
+	parents = arr.filter(i => unique.includes(i['Position']))
+	return parents
+}
+
+function GetHighestParents(arr) {
+	var parents = GetParents(arr)
+	var grandparents = GetParents(parents)
+	while (grandparents.length > 0) {
+		console.log(parents.length)
+		parents = grandparents
+		grandparents = GetParents(parents)
+	}
+	return parents
+}
+
+function BuildNestedArray(arr) {
+    var ancestors = GetHighestParents(arr)
+    ancestors.forEach(a => a.AddChildren(arr))
+    return ancestors
+};
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
 }
